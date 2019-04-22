449c34b5cd5aedb09d398784ff48e0eaf52916d2
Eric Dobbertin <eric@dairystatedesigns.com>
2019-04-22 15:20:25 -0500

    chore: fix eslint issue

--- imports/node-app/core/config.js
+++ imports/node-app/core/config.js
@@ -4,7 +4,7 @@ export default envalid.cleanEnv(process.env, {
   GRAPHQL_INTROSPECTION_ENABLED: bool({ default: false }),
   GRAPHQL_PLAYGROUND_ENABLED: bool({ default: false }),
   MIGRATION_BYPASS_ENABLED: bool({
-    default: false ,
+    default: false,
     desc: "Bypasses migration version checks and migration runs. Enables startup if migration state is not compatible. This can be dangerous enough to cause data inconsistencies. Use at your own risk!"
   }),
   // This is necessary to override the envalid default
